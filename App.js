1d28220f33b4316e8ee9176086446b34c675fff3
hervesson <hervessonporto@hotmail.com>
2021-04-28 21:43:24 -0300

    Criado o Routes e separando as rotas

--- App.js
+++ App.js
@@ -1,112 +1,35 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, { useState, useEffect } from 'react';
+import { View, Text } from 'react-native'
+import auth from '@react-native-firebase/auth';
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import { SignedOutRoutes, SignedInRoutes } from './routes';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+const App = () => {
+	const [initializing, setInitializing] = useState(false);
+   const [user, setUser] = useState();
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+   function onAuthStateChanged(user) {
+    	setUser(user);
+    	if (initializing) setInitializing(false);
+  	}
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  	useEffect(() => {
+    	const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    	return subscriber; // unsubscribe on unmount
+  	}, []);
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>Bora ver, hora do show</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+  	if (initializing) return null;
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+  	if (!user) {
+  		return(
+  			<SignedOutRoutes />	
+  		)
+  	}else{
+  		return (
+    		<SignedInRoutes />
+  		);	
+  	}  	
+}
 
-export default App;
+export default App
--- routes.js
+++ routes.js
@@ -1,29 +1,36 @@
-import { createStackNavigator } from 'react-navigation-stack';
-import { createAppContainer } from 'react-navigation';
-import SideMenu from "./src/Components/SideMenu"
-import Login from "./src/Cenas/Login";
-import Welcome from "./src/Cenas/Welcome";
+import React, { useState, useEffect } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+//signOut
+import Login from "./src/Scenes/Login"
+import Subscribe from "./src/Scenes/Subscribe"
 
 
-import Generico from "./src/Cenas/Generico";
+import AwesomePage from "./src/Scenes/AwesomePage";
 
+const Stack = createStackNavigator();
 
-export const SignedOutRoutes = createStackNavigator({
-  Home: {
-    screen: Login
-  },
-  Welcome: {
-    screen: Welcome
-  }
-});
+export const SignedOutRoutes = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Subscribe" component={Subscribe} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
-export const SignedInRoutes = createStackNavigator(
-  {
-    Generico: {
-    screen: Generico
-    },
-  }
-);
+export const SignedInRoutes = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="AwesomePage">
+        <Stack.Screen name="AwesomePage" component={AwesomePage} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 
 
