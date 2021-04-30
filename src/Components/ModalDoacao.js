f2cd4a8c83f3a802e757f0d83859b6d7f034099a
hervesson <hervessonporto@hotmail.com>
2021-04-30 17:45:47 -0300

    Modal que abriga as cenas de etapas

---
+++ src/Components/ModalDoacao.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import CenaOne from "./CenasModal/CenaOne"
+
+const ModalDoacao = () => {
+   return (
+      <View>
+         <CenaOne />
+      </View>
+   )
+}
+
+export default ModalDoacao
