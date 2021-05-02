87f700f8dde7fdfa0bfc62865fb96bcca5a87171
hervesson <hervessonporto@hotmail.com>
2021-05-02 18:23:13 -0300

    Configurando as rotas

--- src/Components/ModalDoacao.js
+++ src/Components/ModalDoacao.js
@@ -1,13 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 import { View, Text } from 'react-native'
-import CenaOne from "./CenasModal/CenaOne"
+
+import CenaOne from "./CenasModal/CenaOne";
+import CenaTwo from "./CenasModal/CenaTwo"
+import CenaThree from "./CenasModal/CenaThree"
+import CenaFour from "./CenasModal/CenaFour"
 
 const ModalDoacao = () => {
-   return (
-      <View>
-         <CenaOne />
-      </View>
-   )
+	let [activeComponent, setActiveComponent] = useState(<CenaOne callback={callbackFuncao} />);
+	const [componentName, setComponentName] = useState("")
+
+	function callbackFuncao(values){
+		setComponentName(values)
+	};
+
+	useEffect(() => {
+    	switch (componentName) {
+      	case "cenaOne":
+        	   setActiveComponent(<CenaOne callback={callbackFuncao} />);
+         break;
+
+      	case "cenaDois":
+        		setActiveComponent(<CenaTwo callback={callbackFuncao} />);
+        	break;
+
+         case "cenaThree":
+            setActiveComponent(<CenaThree callback={callbackFuncao} />);
+         break;
+
+         case "cenaFour":
+            setActiveComponent(<CenaFour />);
+         break;
+
+      	default:
+        		setActiveComponent(<CenaOne callback={callbackFuncao} />);
+        	break;
+    	}
+  	}, [componentName]);
+
+	return (
+	   <View>
+	   	{activeComponent} 
+	   </View>
+	)
 }
 
 export default ModalDoacao
