de4f32dacef7227d1f9d8cdccaef504520eefa58
hervesson <hervessonporto@hotmail.com>
2021-05-02 18:22:28 -0300

    Deixando o botão fixo

--- src/Components/ButtonDoacao.js
+++ src/Components/ButtonDoacao.js
@@ -8,13 +8,13 @@ const altura = height-30;
 export default class ButtonConversa extends Component {
    render() {
   	   return(
-  			<Animated.View style={styles.animated}>
+  			<View style={styles.animated}>
             <TouchableOpacity style={styles.container} onPress={() => { this.props.callback()}}>
                <Text style={styles.texto}>
                   Fazer Doação
                </Text>
             </TouchableOpacity>
-         </Animated.View>
+         </View>
   	   );
   }
 }
@@ -23,9 +23,9 @@ const styles = StyleSheet.create({
  	animated:{
  		flex: 1,
       paddingTop: 25,
-    	position: "absolute",
+    	//position: "absolute",
     	width,
-    	height: altura,
+    	//height: altura,
     	alignItems: "center",
     	justifyContent: "flex-end",
     },
