e255716e199e74aa0efa5f8688da2fc24af5ccd5
hervesson <hervessonporto@hotmail.com>
2021-05-19 12:22:48 -0300

    Estilizando; Usando formik e criando customer

--- src/Scenes/Subscribe.js
+++ src/Scenes/Subscribe.js
@@ -1,4 +1,291 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { View, Text, TextInput, StyleSheet, SafeAreaView, Button, Image, Dimensions, TouchableOpacity } from 'react-native'
+import auth from '@react-native-firebase/auth';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { Formik } from 'formik'
+import * as yup from 'yup'
+import axios from "axios";
+
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
+const Subscribe = ({navigation}) => {
+
+   function Subscribe(values){
+      auth()
+      .createUserWithEmailAndPassword(values.email, values.password)
+      .then((props) => {
+         var user = auth().currentUser;
+         user.updateProfile({
+            displayName: values.usuario
+         });
+         criarCustomer(props, values.usuario)
+      })
+      .catch(error => {
+         if (error.code === 'auth/email-already-in-use') {
+            console.warn('That email address is already in use!');
+         }
+
+         if (error.code === 'auth/invalid-email') {
+            console.warn('That email address is invalid!');
+         }
+
+         console.error(error);
+      });
+   }
+
+   const criarCustomer = (props, usuario) => {
+      axios.post('https://us-central1-myappdonate.cloudfunctions.net/createCustomer', {
+         email: props.user._user.email,
+         nome: usuario,
+         uid: props.user._user.uid
+      })
+      .then((response) => {
+         console.log(response)
+      })
+      .catch(function (error) {
+         console.log(error);
+      }); 
+   }
+
+
+   const loginValidationSchema = yup.object().shape({
+      email: yup
+         .string()
+         .email("Por favor coloque um email válido!")
+         .required('O endereço de Email é requerido!'),
+      usuario: yup
+         .string()
+         .required('O nome de usuário é requerido!'),
+      password: yup
+         .string()
+         .min(6, ({ min }) => `A senha deve conter no minimo ${min} caracteres`)
+         .required('A senha é requerida!'),
+   })
+
+   return (
+      <SafeAreaView style={{flex: 1}}>
+         <View style={styles.containerSupe}>
+            <Image style={styles.avatar}
+               source={require('../Assets/Images/logoAB.png')}
+            />
+         </View>
+         <View style={styles.containerInfe}>
+            <Text style={{alignSelf: "center", fontFamily: "Open Sans Bold", fontSize: 12, paddingTop: 10, color:"#666" }}>
+               Cadastro com Email e senha:
+            </Text>
+            <View>
+               <Formik
+                  validationSchema={loginValidationSchema}
+                  initialValues={{ email: '', password: '', usuario: ''}}
+                  onSubmit={values => Subscribe(values)}
+               >
+                  {({
+                    handleChange,
+                    handleBlur,
+                    handleSubmit,
+                    values,
+                    errors,
+                    isValid,
+                  }) => (
+                    <>
+                     <View style={styles.input}>
+                        <Icon name="mail-outline" size={20} color="black" />
+                        <TextInput
+                           autoCapitalize="none"
+                           name="email"
+                           placeholder="Endereço de Email"
+                           style={{flex: 1}}
+                           onChangeText={handleChange('email')}
+                           onBlur={handleBlur('email')}
+                           value={values.email}
+                           keyboardType="email-address"
+                        />
+                     </View>   
+                     {errors.email &&
+                        <Text style={{ fontSize: 10, color: 'red', fontFamily: 'Open Sans Regular', paddingLeft:10 }}>{errors.email}</Text>
+                     }
+                     <View style={styles.input}>
+                        <Icon name="person-outline" size={20} color="black" />
+                        <TextInput
+                           autoCapitalize="none"
+                           name="usuario"
+                           placeholder="Nome de usuário"
+                           style={{flex: 1}}
+                           onChangeText={handleChange('usuario')}
+                           onBlur={handleBlur('usuario')}
+                           value={values.usuario}
+                        />
+                     </View>   
+                     {errors.usuario &&
+                        <Text style={{ fontSize: 10, color: 'red', fontFamily: 'Open Sans Regular', paddingLeft:10 }}>{errors.usuario}</Text>
+                     }
+                     <View style={styles.input}>
+                        <Icon name="lock-closed-outline" size={20} color="black" />
+                        <TextInput
+                           autoCapitalize="none"
+                           name="password"
+                           placeholder="Senha"
+                           style={{flex: 1}}
+                           onChangeText={handleChange('password')}
+                           onBlur={handleBlur('password')}
+                           value={values.password}
+                           secureTextEntry
+                        />
+                     </View>    
+                     {errors.password &&
+                        <Text style={{ fontSize: 10, color: 'red', fontFamily: 'Open Sans Regular', paddingLeft:10 }}>{errors.password}</Text>
+                     }
+                  
+                     <TouchableOpacity style={styles.containerAdcCartao} onPress={handleSubmit} disabled={!isValid}>
+                        <Text style={styles.txtAdcCartao}>
+                           Cadastrar-me
+                        </Text>
+                     </TouchableOpacity>
+                    </>
+                  )}
+                </Formik>
+            </View>
+
+           
+
+         </View>
+      </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+   input: {
+      height: 40,
+      flexDirection: "row",
+      paddingLeft: 5,
+      alignItems: "center",
+      marginHorizontal: 12,
+      borderWidth: 1,
+      borderRadius: 10,
+      borderColor: '#707070',
+      marginVertical: 5
+   },
+   forgot: {
+      fontFamily: 'Open Sans Regular',
+      fontSize: 13,
+      alignSelf: 'center',
+      paddingTop: 15 ,
+      textDecorationLine: 'underline'
+   },
+   label: {
+      marginHorizontal: 12,
+      fontSize: 12,
+      fontFamily: 'Open Sans Regular'
+   },
+   containerSupe: {
+      flex: 1,
+      backgroundColor: '#960500',
+      justifyContent: "center",
+      alignItems: "center",
+   },
+   containerInfe: {
+      flex: 2,
+      backgroundColor: 'white',
+      borderTopLeftRadius: 20,
+      borderTopRightRadius: 20,
+      marginTop: -20
+   },
+   avatar: {
+      height: 181,
+      width: 130
+   },
+   containerAdcCartao: {
+      justifyContent: "center",
+      alignItems: "center",
+      height: 55,
+      width: 343,
+      borderRadius: 10,
+      backgroundColor: "#960500",
+      alignSelf: 'center',
+      marginVertical: 10
+   },
+   containerFace: {
+      justifyContent: "center",
+      alignItems: "center",
+      flexDirection: 'row',
+      height: 55,
+      width: 343,
+      borderRadius: 10,
+      backgroundColor: "#3b5998",
+      alignSelf: 'center',
+      marginVertical: 10
+   },
+   containerGoogle: {
+      justifyContent: "center",
+      alignItems: "center",
+      flexDirection: 'row',
+      height: 55,
+      width: 343,
+      borderRadius: 10,
+      backgroundColor: "#c70021",
+      alignSelf: 'center',
+      marginVertical: 10
+   },
+   txtAdcCartao:{
+      fontFamily: "Open Sans ExtraBold",
+      fontSize: 13,
+      color: "#FFF",
+      paddingHorizontal: 10
+   },       
+});
+
+export default Subscribe
+
+
+
+/*<TextInput
+            style={styles.input}
+            onChangeText={setEmail}
+            value={Email}
+            placeholder="Email"
+         />
+         <TextInput
+            style={styles.input}
+            onChangeText={setSenha}
+            value={Senha}
+            placeholder="Senha"
+            keyboardType="numeric"
+         />
+         <Button title="Logar" color="green" onPress={() => {Logar()}}/>
+         <Text>Ainda não possui cadastro, cadastre-se clicando no botão abaixo!</Text>
+         <Button  title="Cadastre-se" color="blue" onPress={() => {navigation.navigate('Subscribe')}}/>*/
+
+         {/*<Text style={styles.label}>
+                  Email:
+               </Text>}
+               <View style={styles.input}>
+                  <Icon name="mail-outline" size={20} color="black" />
+                  <TextInput
+                     style={{flex: 1}}
+                     onChangeText={setEmail}
+                     value={Email}
+                     placeholder="Email"
+                  />
+               </View>*/
+               /*<Text style={styles.label}>
+                  Senha:
+               </Text>}
+               <View style={styles.input}>
+                  <Icon name="lock-closed-outline" size={20} color="black" />
+                  <TextInput
+                     style={{flex: 1}}
+                     onChangeText={setSenha}
+                     value={Senha}
+                     placeholder="Senha"
+                     //keyboardType="numeric"
+                  />
+               </View>*/}
+
+
+
+{/*import React, { useState } from 'react'
 import { View, Text, TextInput, StyleSheet, SafeAreaView, Button } from 'react-native'
 import auth from '@react-native-firebase/auth';
 
@@ -53,4 +340,4 @@ const styles = StyleSheet.create({
    },
 });
 
-export default Subscribe
+export default Subscribe*/}
