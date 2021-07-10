01ce1891b5a7cff56c06b697442776ebf5261ad9
hervesson <34557380+hervesson@users.noreply.github.com>
2021-07-10 17:08:31 -0300

    Buscando endereço automaticamente

--- src/Components/CadastroModal/CadastroBeneficiario4.js
+++ src/Components/CadastroModal/CadastroBeneficiario4.js
@@ -1,111 +1,145 @@
-import React from 'react'
-import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, Alert, ScrollView } from 'react-native'
 import Icon from 'react-native-vector-icons/Ionicons';
-import { Formik } from 'formik'
-import * as yup from 'yup'
+import axios from "axios";
+
+import { TextInputMask } from 'react-native-masked-text';
 
 import StepByStep from "../StepByStep"
 
 const CadastroBeneficiario4 = (props) => {
+   const [cep, setCep] = useState("")
+   const [uf, setUf] = useState("")
+   const [localidade, setLocalidade] = useState("")
+   const [bairro, setBairro] = useState("")
+   const [logradouro, setLogradouro] = useState("")
+   const [complemento, setComplemento] = useState("")
+   const [errorCep, seErrorCep] = useState(false)
+
+
+   const BucarCep = (text) => {
+      setCep(text);
+      if(text.length == 9){
+         axios.get("https://viacep.com.br/ws/"+text+"/json/").then(response => {
+            if(response.data.cep == text){
+               setCep(response.data.cep), 
+               setUf(response.data.uf)
+               setLocalidade(response.data.localidade)
+               setBairro(response.data.bairro)
+               setLogradouro(response.data.logradouro)
+               setComplemento(response.data.complemento)
+               seErrorCep(false)
+            }else{
+               seErrorCep("Cep não encontrado")
+            }  
+         });
+      }
+   }
+
+   
+
 	return (
-		<View style={{flex: 1}}>
+		<ScrollView style={{flex: 1}}>
 			<View style={{flex: 1}}>
 				<Text style={styles.titulo}>Cadastro Beneficiário</Text>
 				<StepByStep corOne="#e9e9e9" corTwo="#e9e9e9" corThree="#960500" />
 				<Text style={{fontFamily: 'Open Sans Regular', marginTop: 25, textAlign: "center" }}>Dados de Endereço</Text>
 				<View style={{marginTop: 25}}>
-            <Formik
-               //validationSchema={cadastroValidationSchema}
-               initialValues={{ cep: '', endereco: '', bairro: '', cidade: ''}}
-               onSubmit={values => props.callback("sucess")}
-            >
-            {({
-               handleChange,
-               handleBlur,
-               handleSubmit,
-               values,
-               errors,
-               isValid,
-                  }) => (
-                    <>
-                     <View style={styles.input}>
-                        <TextInput
-                           autoCapitalize="none"
-                           name="cep"
-                           placeholder="CEP"
-                           style={{flex: 1}}
-                           onChangeText={handleChange('cep')}
-                           onBlur={handleBlur('cep')}
-                           value={values.cep}
-                        />
-                     </View>   
-                     {errors.cep &&
-                        <Text style={styles.erros}>{errors.cep}</Text>
-                     }
-                     <View style={styles.input}>
-                        <TextInput
-                           autoCapitalize="none"
-                           name="endereco"
-                           placeholder="Endereço"
-                           style={{flex: 1}}
-                           onChangeText={handleChange('endereco')}
-                           onBlur={handleBlur('endereco')}
-                           value={values.endereco} 
-                        />
-                     </View>    
-                     {errors.endereco &&
-                        <Text style={styles.erros}>{errors.endereco}</Text>
-                     }
+               <View style={styles.input}>
+                  <TextInputMask
+                     type={"custom"}
+                     options={{ mask: "99999-999" }}
+                     placeholder="CEP"
+                     style={{flex: 1}}
+                     onChangeText={(text) => BucarCep(text)}
+                     maxLength={9}
+                     keyboardType="numeric"
+                     value={cep}
+                  />
+               </View>  
+               {errorCep &&
+                  <Text style={styles.erros}>{errorCep}</Text>
+               }
 
-                     <View style={styles.input}>
-                        <TextInput
-                           autoCapitalize="none"
-                           name="Bairro"
-                           placeholder="bairro"
-                           style={{flex: 1}}
-                           onChangeText={handleChange('bairro')}
-                           onBlur={handleBlur('bairro')}
-                           value={values.bairro}
-                        />
-                     </View>    
-                     {errors.bairro &&
-                        <Text style={styles.erros}>{errors.bairro}</Text>
-                     }
+               <View style={styles.input}>
+                  <TextInput
+                     autoCapitalize="none"
+                     name="endereco"
+                     placeholder="Endereço"
+                     style={{flex: 1}}
+                     //onChangeText={handleChange('endereco')}
+                     //onBlur={handleBlur('endereco')}
+                     value={logradouro} 
+                     editable={false}
+                  />
+               </View>    
+               {/*errors.endereco &&
+                  <Text style={styles.erros}>{errors.endereco}</Text>
+               */}
 
-                     <View style={styles.input}>
-                        <TextInput
-                           autoCapitalize="none"
-                           name="cidade"
-                           placeholder="Cidade"
-                           style={{flex: 1}}
-                           onChangeText={handleChange('cidade')}
-                           onBlur={handleBlur('cidade')}
-                           value={values.cidade}
-                        />
-                     </View>    
-                     {errors.cidade &&
-                        <Text style={styles.erros}>{errors.cidade}</Text>
-                     }
+               <View style={styles.input}>
+                  <TextInput
+                     autoCapitalize="none"
+                     name="Bairro"
+                     placeholder="bairro"
+                     style={{flex: 1}}
+                     //onChangeText={handleChange('bairro')}
+                     //onBlur={handleBlur('bairro')}
+                     value={bairro}
+                     editable={false}
+                  />
+               </View>    
+               {/*errors.bairro &&
+                  <Text style={styles.erros}>{errors.bairro}</Text>
+               */}
 
-                     <View style={styles.containerIput}>
-								<Icon name="cloud-upload" size={30} color="black" />
-								<TouchableOpacity style={styles.input1}>
-									<Text style={styles.placeholder}>Foto da RG ou CNH</Text>
-								</TouchableOpacity>
-							</View>
+               <View style={styles.input}>
+                  <TextInput
+                     autoCapitalize="none"
+                     name="cidade"
+                     placeholder="Cidade"
+                     style={{flex: 1}}
+                     //onChangeText={handleChange('cidade')}
+                     //onBlur={handleBlur('cidade')}
+                     value={localidade}
+                     editable={false}
+                  />
+               </View>    
+               {/*errors.cidade &&
+                  <Text style={styles.erros}>{errors.cidade}</Text>
+               */}
 
-                     <TouchableOpacity style={styles.containerAdcCartao} onPress={handleSubmit} disabled={!isValid}>
-                        <Text style={styles.txtAdcCartao}>
-									Cadastrar                          
-                        </Text>
-                     </TouchableOpacity>
+               <View style={styles.input}>
+                  <TextInput
+                     autoCapitalize="none"
+                     name="estado"
+                     placeholder="Estado"
+                     style={{flex: 1}}
+                     //onChangeText={handleChange('estado')}
+                     //onBlur={handleBlur('estado')}
+                     value={uf}
+                     editable={false}
+                  />
+               </View>    
+               {/*errors.cidade &&
+                  <Text style={styles.erros}>{errors.cidade}</Text>
+               */}
 
-                    </>
-                  )}
-               </Formik>
+               <View style={styles.containerIput}>
+						<Icon name="cloud-upload" size={30} color="black" />
+						<TouchableOpacity style={styles.input1}>
+						   <Text style={styles.placeholder}>Foto do comprovente de residência</Text>
+						</TouchableOpacity>
+					</View>
+
+               <TouchableOpacity style={styles.containerAdcCartao} onPress={() => props.callback("sucess")}>
+                  <Text style={styles.txtAdcCartao}>
+							Cadastrar                          
+                  </Text>
+               </TouchableOpacity>
             </View>
 			</View>
-		</View>
+		</ScrollView>
 	)
 }
 
