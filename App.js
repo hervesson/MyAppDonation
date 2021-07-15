485f445de50c3ee710806cdc2604fa5019ff4380
hervesson <34557380+hervesson@users.noreply.github.com>
2021-07-15 20:16:22 -0300

    Instalado Reactotron

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { View, Text } from 'react-native'
 import auth from '@react-native-firebase/auth';
 
+import('./ReactotronConfig').then(() => console.log('Reactotron Configured'))
+
 import { SignedOutRoutes, SignedInRoutes } from './routes';
 
 
